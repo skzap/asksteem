1de2f9d7952668eb12987d0b8755f845466a6878
Adrien M <adrien@d.tube>
2018-01-25 23:20:45 +0100

    add key to search

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ AskSteem = ApiWrapper.create({
    root: 'https://api.asksteem.com/',
    get: {
        suggestions: '/suggestions?${term}',
-       search: '/search?q=${q}&pg=${pg}&include=${include}&sort_by=${sort_by}&order=${order}&types=${types}',
+       search: '/search?q=${q}&pg=${pg}&include=${include}&sort_by=${sort_by}&order=${order}&types=${types}&key=${key}',
        related: '/related?author=${author}&permlink=${permlink}&q=${q}&user=${user}&include=${include}&sort_by=${sort_by}&order=${order}',
        trending: '/trending?q=${q}&size=${size}&types=${types}',
    }
